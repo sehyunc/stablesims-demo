3a4bc7039841eadaa6de9cd5a0b1438ad8adee4f
Sehyun Chung <sehyun@berkeley.edu>
2020-12-03 12:33:39 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Template with Snowpack, ESLint, Prettier</h1>
+    </div>
+  );
+}
+
+export default App;
